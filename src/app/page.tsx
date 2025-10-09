cf10a7e16a255ea543bb405add22243ee0b1719d
Matheus Mattos <matheusmattos.r@gmail.com>
2025-10-09 13:54:56 -0300

    feat: create ProductPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import ProductImage from "./components/ProductImage";
+import ProductInfo from "./components/ProductInfo";
+
 export default function Home() {
-  return;
+  return (
+    <main className="grid grid-cols-1 p-5 lg:grid-cols-12 lg:p-0 lg:mt-6 lg:gap-4">
+      <ProductImage />
+      <ProductInfo />
+    </main>
+  );
 }
