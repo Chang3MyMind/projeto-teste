c81d4904f55a8324f21e224c711cb3d20ec53790
Matheus Mattos <matheusmattos.r@gmail.com>
2025-10-09 13:54:38 -0300

    feat: create Navigation componente

---
+++ src/app/components/Navigation.tsx
@@ -0,0 +1,11 @@
+export default function Navigation() {
+  return (
+    <nav className="p-1">
+      <ul className="flex flex-row flex-wrap gap-2 text-xs sm:text-sm">
+        <li className="text-black">Home</li>
+        <li className="text-black">Products</li>
+        <li className="text-gray-400">Sneakers</li>
+      </ul>
+    </nav>
+  );
+}
