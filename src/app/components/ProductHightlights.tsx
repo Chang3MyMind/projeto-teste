5769316fc1b48c4c1f66bd9394fcb5fef649564d
Matheus Mattos <matheusmattos.r@gmail.com>
2025-10-09 13:54:04 -0300

    feat: create ProductHighlights componente

---
+++ src/app/components/ProductHightlights.tsx
@@ -0,0 +1,16 @@
+export default function ProductHightlights() {
+  return (
+    <>
+      <h3 className="font-semibold text-lg sm:text-xl">Hightlights:</h3>
+      <ul className="list-disc p-3 marker:text-xs ml-4 sm:ml-6 space-y-2">
+        <li className="mb-3 text-sm sm:text-base">
+          Lightweight and Responsive Midsole.
+        </li>
+        <li className="mb-3 text-sm sm:text-base">
+          Full-Length Zoom Air Unit.
+        </li>
+        <li className="mb-3 text-sm sm:text-base">Breathable Mesh Uppe</li>
+      </ul>
+    </>
+  );
+}
